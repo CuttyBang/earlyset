2dc949abbcab2ba74f40dea5e454f7ed945c7dc3
Nathan McElwain <nathanmcelwain@ymail.com>
2016-12-11 13:49:53 -0800

    initial commit

---
+++ _build/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(()=> {
+  'use strict'
+  let mainFunc = (e)=> {
+    e.preventDefault();
+    console.log(e);
+  };
+  mainFunc();
+});
---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){var e=function(e){e.preventDefault(),console.log(e)};e()});
